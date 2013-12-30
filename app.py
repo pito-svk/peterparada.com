39dccb3d9101a7957c9d98af1110a23d60bcee50
pito-svk <parad.peter@gmail.com>
2013-12-30 22:06:59 +0100

    small changes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import os
 
-# Create application
+
 app = Flask(__name__, static_folder='static', static_url_path='',
             template_folder='static')
 
